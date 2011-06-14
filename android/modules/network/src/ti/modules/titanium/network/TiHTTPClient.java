d253f96d6da50c1b4fc9883a4f77c972ca25eed1
Christoph Hauzenberger <chris@memonic.com>
2011-06-14 10:33:55 +0200

    Using GZIP compresssion

--- android/modules/network/src/ti/modules/titanium/network/TiHTTPClient.java
+++ android/modules/network/src/ti/modules/titanium/network/TiHTTPClient.java
@@ -23,13 +23,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.zip.GZIPInputStream;
 
 import org.apache.http.Header;
+import org.apache.http.HeaderElement;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.HttpVersion;
 import org.apache.http.MethodNotSupportedException;
 import org.apache.http.NameValuePair;
@@ -50,6 +54,7 @@ import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.scheme.SocketFactory;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.AbstractHttpEntity;
+import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ContentBody;
@@ -108,6 +113,9 @@ public class TiHTTPClient
 	private static final String ON_ERROR = "onerror";
 	private static final String ON_DATA_STREAM = "ondatastream";
 	private static final String ON_SEND_STREAM = "onsendstream";
+	
+	private static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
+    private static final String ENCODING_GZIP = "gzip";
 
 	private static DefaultHttpClient client;
 	
@@ -446,6 +454,31 @@ public class TiHTTPClient
 			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
 
 			client = new DefaultHttpClient(new ThreadSafeClientConnManager(params, registry), params);
+			// add gzip support
+			client.addRequestInterceptor(new HttpRequestInterceptor() {
+              public void process(HttpRequest request, HttpContext context) {
+                // Add header to accept gzip content
+                if (!request.containsHeader(HEADER_ACCEPT_ENCODING)) {
+                  request.addHeader(HEADER_ACCEPT_ENCODING, ENCODING_GZIP);
+                }
+              }
+            });
+
+            client.addResponseInterceptor(new HttpResponseInterceptor() {
+              public void process(HttpResponse response, HttpContext context) {
+                // Inflate any responses compressed with gzip
+                final HttpEntity entity = response.getEntity();
+                final Header encoding = entity.getContentEncoding();
+                if (encoding != null) {
+                  for (HeaderElement element : encoding.getElements()) {
+                    if (element.getName().equalsIgnoreCase(ENCODING_GZIP)) {
+                      response.setEntity(new InflatingEntity(response.getEntity()));
+                      break;
+                    }
+                  }
+                }
+              }
+            });
 		}
 	}
 
@@ -1091,4 +1124,24 @@ public class TiHTTPClient
 	{
 		return autoEncodeUrl;
 	}
+	
+	/**
+    * Simple {@link HttpEntityWrapper} that inflates the wrapped
+    * {@link HttpEntity} by passing it through {@link GZIPInputStream}.
+    */
+    private static class InflatingEntity extends HttpEntityWrapper {
+        public InflatingEntity(HttpEntity wrapped) {
+            super(wrapped);
+        }
+
+        @Override
+        public InputStream getContent() throws IOException {
+            return new GZIPInputStream(wrappedEntity.getContent());
+        }
+
+        @Override
+        public long getContentLength() {
+            return -1;
+        }
+    }
 }
